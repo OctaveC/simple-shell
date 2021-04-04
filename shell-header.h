801de3c47b81599f3cb7c5ebe9c88645afef79b3
OctaveC <octavec@outlook.com>
2021-04-04 14:48:48 +0000

    added header file

---
+++ shell-header.h
@@ -0,0 +1,26 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+typedef struct list_s
+{
+	char *data;
+	struct list_s *next;
+	struct list_s *prev;
+
+} list_t;
+
+int _strlen(const char *s);
+char *_strdup(char *str);
+char *_getenv(char *name);
+size_t print_list(const list_t *h);
+list_t *createNodeList(list_t **head, const char *str);
+int _setenv(char *name, const char *value, int overwrite);
+int _unsetenv(char *name);
+
+#endif /* SHELL_H */
