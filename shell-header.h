80deb86375ea7d56ea7ce8c43adf503bada36e64
Sarolus <cactussius.primus@gmail.com>
2021-04-06 15:17:13 +0200

    Include str functions

--- shell-header.h
+++ shell-header.h
@@ -19,9 +19,13 @@ typedef struct list_s
 extern char **environ;
 
 char *_which(char *argv[], char *str);
-int _strlen(const char *s);
+int _strlen(char *s);
 char *_strdup(char *str);
 char *_getenv(char *name);
+char *_strtok(char *str, char *sep, char **saveptr);
+unsigned int _strspn(char *s1, char *s2);
+unsigned int _strcspn(char *s1, char *s2);
+char *_strchr(char *str, char c);
 size_t print_list(const list_t *h);
 list_t *createNodeList(list_t **head, const char *str);
 int _setenv(char *name, const char *value, int overwrite);
---
+++ string_func.c
@@ -0,0 +1,137 @@
+#include "shell-header.h"
+
+/**
+ * 
+ * 
+ */
+char *_strtok(char *str, char *sep, char **saveptr)
+{
+	char *end = NULL;
+
+	if (str == NULL)
+	{
+		str = *saveptr;
+	}
+
+	if (*str == '\0')
+	{
+		*saveptr = str;
+		return (NULL);
+	}
+
+	str += _strspn(str, sep);
+
+	if (*str == '\0')
+	{
+		*saveptr = str;
+		return (NULL);
+	}
+
+	end = str + _strcspn(str, sep);
+
+	if (*end == '\0')
+	{
+		*saveptr = end;
+		return (str);
+	}
+
+	*end = '\0';
+	*saveptr = end + 1;
+	return (str);
+}
+
+/**
+ * _strspn - return the length of the substring that contain
+ * only accepted characters.
+ * @s1: string to check.
+ * @s2: reference string
+ * Return: return the length if success otherwise NULL.
+ */
+unsigned int _strspn(char *s1, char *s2)
+{
+	unsigned int len = 0;
+
+	while (s1[len])
+	/* return s1 char position if found in s2 using strchr */
+	{
+		if (_strchr(s2, s1[len]) != NULL)
+		{
+			len++;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return (len);
+}
+
+/**
+ * _strcspn - return the length of the substring that do not contain
+ * the rejected characters.
+ * @s1: string to check
+ * @s2: reference string
+ * Return: return the length if success otherwise NULL.
+ */
+unsigned int _strcspn(char *s1, char *s2)
+{
+	unsigned int len = 0;
+
+	while (s1[len]) /* till it's different from '\0' */
+	{
+		if (_strchr(s2, s1[len]) != NULL)
+		{
+			return (len); /* return s1 char position if found in s2 */
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return (len);
+}
+
+/**
+ * _strchr - check for specified character in a string
+ * @str: the string
+ * @c: the specified character
+ * Return: return our string if true, otherwise NULL if false.
+ */
+char *_strchr(char *str, char c)
+{
+	while (*str != '\0')
+	{
+		if (*str == c)
+		{
+			return (str);
+		}
+		str++;
+	}
+
+	if (*str == c)
+	{
+		return (str);
+	}
+
+	return (NULL);
+}
+
+/**
+ * _strlen - Returns lenght of a string.
+ * @s: string
+ * Return: Return result
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
