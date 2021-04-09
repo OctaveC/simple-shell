a4ec455db232ce1a985ea2b21a5d6e03cb8700e0
OctaveC <octavec@outlook.com>
2021-04-09 21:09:20 +0000

    cleaned up directory

--- simple-shell.c
+++ simple-shell.c
@@ -1,5 +1,108 @@
 #include "shell-header.h"
 
+void all_execves(char **token_array, char *name)
+{
+	char *str, *str2;
+	char *truc;
+	int ite3 = 0;
+
+	str = getenv("PATH");
+
+	str2 = strdup(str);
+
+	truc = _which(token_array, str2);
+
+	free(str2);
+
+	if (execve(truc, token_array, environ) == -1)
+	{
+		while (token_array[ite3])
+		{
+			free(token_array[ite3]);
+			ite3++;
+		}
+		free(token_array);
+
+		perror(name);
+
+		exit(0);
+	}
+}
+
+void create_child(pid_t pids[], int *ite, char **token_array, char *name)
+{
+	int status;
+
+	pids[*(ite)] = fork();
+
+	if (pids[*(ite)] == -1)
+	{
+		perror(name);
+		exit(0);
+	}
+	else if (pids[*(ite)] == 0)
+		all_execves(token_array, name);
+	else
+		waitpid(pids[*(ite)], &status, WUNTRACED);
+}
+
+void getline_strtok_and_fork(int *ite, pid_t pids[], char *name)
+{
+	void (*f)(int);
+	char *token, *str;
+	int ite2, ite3 = 0, len = 0;
+
+	char **token_array, *saveptr, *buffer;
+
+	buffer = _getline();
+
+	token_array = calloc(sizeof(char *), 10);
+
+	while (buffer[len] != '\0')
+	{
+		len++;
+	}
+	if (buffer[len - 1] == '\n')
+	{
+		buffer[len - 1] = '\0';
+	}
+
+	str = buffer;
+
+	if ((f = check_builtin(str)))
+  {
+		f(0);
+	}
+
+	for (ite2 = 0;; ite2++, str = NULL)
+	{
+		token = _strtok(str, " \t", &saveptr);
+		if (token == NULL)
+		{
+			free(buffer);
+			break;
+		}
+		token_array[ite2] = calloc(sizeof(char), 200);
+		strcat(token_array[ite2], token);
+	}
+
+	create_child(pids, ite, token_array, name);
+
+	ite3 = 0;
+	while (ite3 < ite2)
+	{
+		free(token_array[ite3]);
+		ite3++;
+	}
+	free(token_array);
+}
+
+void CtrlC(int i)
+{
+	i = i;
+	write(1, "\n$ ", 3);
+}
+
 /**
  * main - execv+fork+wait demonstration (WIP)
  *
@@ -7,55 +110,17 @@
  */
 int main(int argc, char *argv[])
 {
-	pid_t pids[5];
-	char *buffer = NULL, *str, *token;
-	size_t buffsize = 0;
 	int ite = 0;
-	int ite2, linenum = -1;
-	int status;
-	pid_t pid;
-	char *token_array[50];
-	char *saveptr;
+	pid_t pids[20];
+	char *name = argv[0];
 
-	/* Start children */
-	for (;;)
+	signal(SIGINT, CtrlC);
+
+	for (ite = 0;; ite++)
 	{
-		buffer = NULL;
-		write(1, "$ ", 2);
-		if (getline(&buffer, &buffsize, stdin) == EOF)
-		{
-			free(buffer);
-			write(STDIN_FILENO, "\n", 1);
-			break;
-		}
-		if ((pids[ite] = fork()) < 0)
-		{
-			perror("fuck");
-			return (1);
-		}
-		else if (pids[ite] == 0)
-		{
-			for (ite2 = 0, str = buffer;; ite2++, str = NULL)
-			{
-				token = strtok_r(str, " \n", &saveptr);
-				if (token == NULL)
-					break;
-				token_array[ite2] = token;
-			}
-			token_array[ite2] = NULL;
-			if (execve(token_array[0], token_array, NULL) == -1)
-			{
-				free(buffer);
-				perror(argv[0]);
-			}
-			exit(0);
-		}
-
-		/* Wait for children to exit */
-		pid = wait(&status);
-		pid = pid;
-		free(buffer);
+		write(STDIN_FILENO, "$ ", 2);
+		getline_strtok_and_fork(&ite, &pids[20], name);
 	}
 	argc = argc;
-	return (linenum);
+	return (0);
 }
